8d55ce0450280ac044c84ff8cad08d8a348773fd
Nicholas Moyer <nicholas.moyer@aksteel.com>
2018-05-16 10:17:39 -0400

    Added resource files to master

---
+++ res/people.js
@@ -0,0 +1,5 @@
+	var LV1 = ["doug.moyer", "tim.richardson"];
+	var LV2 = ["nicholas.moyer", "james.fellure"];
+	var LV3 = ["ethan.dishman"];
+	var PRD = ["dave.gardner", "henry.wooley", "rob.huff"];
+	var TEC = ["mike.ruhl", "john.keogh", "michael.moore", "tom.adkins", "ken.haering"];
