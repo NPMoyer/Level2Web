673ecef2bc070eefc87a97512f643fff21f0905e
Nicholas Moyer <nicholas.moyer@aksteel.com>
2018-07-02 14:45:52 -0400

    Added network status of level 2 nodes to index

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var http = require('http');
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var ping = require('ping');
 
 app.use(express.static('public'));
 
@@ -7,7 +11,21 @@ app.get('/', function (req, res) {
     res.redirect('html/Index.htm');
 });
 
-var server = app.listen(8000, function () {
+var host2 = ['MSODT2', 'MSODT3', 'MSOHSM', 'MSOAOD', 'MSOEAF'];
+
+var frequency = 15000; //15 seconds
+
+host2.forEach(function(host){
+    setInterval(function() {
+        ping.sys.probe(host, function(active){
+            var info = active ? host + ' = Online' : host + ' = Offline';
+            io.sockets.emit('update-msg', info);
+            console.log(info);
+        });
+    }, frequency);
+});
+
+server.listen(8000, function () {
 
     var host = server.address().address;
     var port = server.address().port;
