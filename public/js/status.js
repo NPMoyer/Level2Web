18d42e02235251d5c6813c53e9c943557383280f
Nicholas Moyer <nicholas.moyer@aksteel.com>
2018-07-09 07:56:43 -0400

    Cleaned up server status code and added background

---
+++ public/js/status.js
@@ -0,0 +1,24 @@
+$(document).ready(connection());
+
+function connection(){
+    var socket = io.connect('http://172.17.230.18:8000');
+
+    var i = 0;
+
+    socket.on('update-msg', function (msg) {	
+        if (i == 0){
+            $('#results').html('<tr><th>MSODT2</th><th>MSODT3</th><th>MSOHSM</th>' + 
+                '<th>MSOHSA</th><th>MSOCC1</th><th>MSOAOD</th><th>MSOEAF</th></tr><tr>');
+        }
+
+        $('#results').append('<td>' + msg);
+        i++;
+
+        if (i != 7){
+            $('#results').append('</td>');			
+        } else {
+            i = 0;
+            $('#results').append('</td></tr>');
+        }
+    });
+}
