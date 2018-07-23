fc87a82a4202d960590dd904d714528502f02c6f
Nicholas Moyer <nicholas.moyer@aksteel.com>
2018-07-23 10:26:15 -0400

    Added winston for file logging

--- server.js
+++ server.js
@@ -1,44 +1,85 @@
 var express = require('express');
 var http = require('http');
-var app = express();
-var server = http.createServer(app);
+var fs = require('fs');
 var io = require('socket.io').listen(server);
 var ping = require('ping');
 var MongoClient = require('mongodb').MongoClient;
+var winston = require('winston');
+require('winston-daily-rotate-file');
+
+var app = express();
+var server = http.createServer(app);
+var date = new Date();
 var url = "mongodb://localhost:27017/";
-
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-    res.redirect('html/index.htm');
-});
-
+var logDir = '//mamafil01/automatn_pub/level 2 web/logs';
 var hostNames = ['MSODT2', 'MSODT3', 'MSOHSM', 'MSOHSA', 'MSOCC1', 'MSOAOD', 'MSOEAF'];
+
 var frequencyPing = 30000; //30 seconds
 var frequencyUsers = 10000; //10 seconds
+
 var nspIndex = io.of('/index');
 var nspChat = io.of('/chat');
 var nspEmail = io.of('/email');
 
+// For todays date;
+Date.prototype.today = function () { 
+    return (((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+ this.getFullYear();
+};
+// For the time now
+Date.prototype.timeNow = function () {
+    return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + 
+    this.getSeconds();
+};
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
+
+// Create the logger
+var transport = new (winston.transports.DailyRotateFile)({
+    filename: logDir + '/%DATE%.log',
+    datePattern: 'MM-DD-YYYY',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '14d'
+});
+var logger = winston.createLogger({
+    transports: [
+        transport
+    ]
+});
+logger.info(date.today() + " " + date.timeNow() + " " + 'Start of logging');
+
+
+// Specify the location for source code
+app.use(express.static('public'));
+
+// Redirect the client to the index page
+app.get('/', function (req, res) {
+    res.redirect('html/index.htm');
+});
+
 users = [];
 ids = [];
 
+// Update index.htm with status of servers
 hostNames.forEach(function(host){
     setInterval(function() {
-        // Update index.htm with status of servers
         ping.sys.probe(host, function(active){
             var info = active ? 'Online' : 'Offline';
+            if (info == 'Offline')
+                logger.warn(date.today() + " " + date.timeNow() + " " + host + ":" + info);
             nspIndex.emit('update-msg', info);
         });
     }, frequencyPing);
 });
 
 nspChat.on('connection', function(socket)  {
-    var date = new Date();
     var ip = socket.request.connection.remoteAddress;
 
+    //  Update chat.htm with users currently connected
     setInterval(function() {
-        //  Update chat.htm with users currently connected
         nspChat.emit('usersActive', users);
     }, frequencyUsers);
 
@@ -65,28 +106,18 @@ nspChat.on('connection', function(socket)  {
         });
     });
 
-    // For todays date;
-    Date.prototype.today = function () { 
-        return (((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+ this.getFullYear();
-    };
-
-    // For the time now
-    Date.prototype.timeNow = function () {
-        return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
-    };
-
     // Log the connection
-    console.log(date.today() + " " + date.timeNow() + " " + 'New connection from ' + ip);
+    logger.info(date.today() + " " + date.timeNow() + " " + 'New connection from ' + ip);
 
-    // User has set their username
     socket.on('setUsername', function(data) {    
+        // User has set their username
         if(users.indexOf(data) > -1) {
             socket.emit('userExists', data + ' username is taken! Try some other username.');
         } else {
             users.push(data);
             ids.push(socket.id);
             socket.emit('userSet', {username: data});
-            console.log(ip + " set as " + data);
+            loggger.info(ip + " set as " + data);
             nspChat.emit('connected', data);
         }
     });
@@ -94,7 +125,7 @@ nspChat.on('connection', function(socket)  {
     socket.on('msg', function(data) {
         //Send message to everyone
         nspChat.emit('newmsg', data);
-        console.log(data.user + ": " + data.message);
+        logger.info(data.user + ": " + data.message);
     });
 
     socket.on('disconnect', function() {
@@ -102,7 +133,7 @@ nspChat.on('connection', function(socket)  {
         var index = ids.indexOf(socket.id);
         var user = users[index];
         nspChat.emit('disconnected', user);
-        console.log(user + ' disconnected');
+        logger.info(date.today() + " " + date.timeNow() + " " + user + ' disconnected');
         ids.splice(index, 1);
         users.splice(index, 1);
     });
